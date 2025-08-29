d3bc44a722a9f222d23155120a1aae1259ff0749
draggner <devdraggnerstudio@gmail.com>
2025-08-29 01:07:54 -0300

    c: draft of developing the guessing game in C

---
+++ guess.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //printf, puts, scanf
+#include <stdlib.h> //srand, rand
+#include <time.h> // time
+
+
+int main() {
+
+  srand(time(NULL));
+
+  int number = rand();
+  int entry;
+
+  printf("Adivinhe o Número\n");
+  scanf("%d", &entry);
+
+  if (entry == number){
+    printf("Acertou!\n");
+  } else {
+    printf("Errou, é %d\n", number);
+  }
+
+  return 0;
+}
