2692e2e87d19e0c4bad3bcef24e8a27489a9d11c
draggner <devdraggnerstudio@gmail.com>
2025-08-31 23:03:41 -0300

    c: Standard modifications of the simples calculator using do-while

---
+++ calcInterative.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+
+  int number1, number2, operation;
+
+  puts("Calculadora Simples - v1.1");
+  puts("References - Tales");
+  puts("by Draggner");
+
+  do {
+    printf("Informe o primeiro número:\n");
+    scanf("%d", &number1);
+
+    puts("\n Estas são as operações possíveis:\n");
+    puts("\t 1. Somar");
+    puts("\t 2. Subtração");
+    puts("\t 3. Multiplicação");
+    puts("\t 4. Divisão");
+
+    puts("\t 5. Sair");
+
+    puts(">>");
+    scanf("%d", &operation);
+
+    printf("Informe o segundo número:\n");
+    scanf("%d", &number2);
+
+    if (operation == 1) {
+     printf("%d + %d = %d\n", number1, number2, (number1 + number2));
+    } else if (operation == 2) {
+     printf("%d - %d = %d\n", number1, number2, (number1 - number2));
+    } else if (operation == 3) {
+     printf("%d * %d = %d\n", number1, number2, (number1 * number2));
+    } else if (operation == 4) {
+     printf("%d / %d = %d\n", number1, number2, (number1 / number2));
+    } else if (operation == 5) {
+     ;
+    } else puts("OPERAÇÃO INVÁLIDA, TENTE NOVAMENTE!");
+  } while (operation != 5);
+
+  return 0;
+}
